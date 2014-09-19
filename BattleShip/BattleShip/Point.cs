e22608e903f461f8d17260afaec3b5e1c13c232c
Alemshewit <alemshewitweldearegay@gmail.com>
2014-09-19 16:45:14 -0600

    NewFile

---
+++ BattleShip/BattleShip/Point.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BattleShip
+{
+    class Point
+    {
+        public enum Pointstatus
+        {
+            Empty,
+            Ship,
+            Hit,
+            Miss
+        }
+
+        public int X {get; set;}
+        public int Y {get; set;}
+        public Pointstatus Status { get; set; }
+
+        //consotructor
+        public Point(int x, int y, Pointstatus p)
+        {
+            this.X = x;       
+            this.Y = y;
+            this.Status = p;
+        }
+
+
+
+    }
+}
