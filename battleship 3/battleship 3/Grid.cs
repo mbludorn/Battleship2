040cc846805b51b743ba87cbe91f614db2f0d67e
SeedPaths-User <lessdismissive@email.com>
2014-09-22 11:34:25 -0600

    battleship

---
+++ battleship 3/battleship 3/Grid.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BattleShip
+{
+    class Grid
+    {
+        public enum PlaceShipDirection
+        {
+            Horizontal,
+            Vertical
+        }
+        //define properties
+        public Point[,] Ocean {get; set;}
+        public List<Ship> ListOfShips {get; set;}
+        public bool AllShipsDestroyed {get {return this.}
+        public int CombatRound {get; set;}
+
+    //define constructor
+    public Grid()
+    {
+        //itialize the ocean   
+        this.Ocean = new Point [10, 10];
+        //this.Ocean[x, y] = new Point(x, y, Point.Pointstatus.Empty); 
+        this.ListOfShips =  new List<Ship>();
+        //this.ListOfShips.Add(
+
+    }
+
+     public void PlaceShip(Ship shipToPlace, PlaceShipDirection direction, int startX, int startY)
+     {
+         for (int i = 0; i < shipToPlace.Length; i++)
+         {	
+           Point thePoint = Ocean[startX, startY]; 
+              // Point.Pointstatus = Ship.ShipType;
+             thePoint.Status = Point.Pointstatus.Ship;
+
+             shipToPlace.OccupiedPoints.Add(thePoint);
+
+             if(direction == PlaceShipDirection.Horizontal)
+               {
+                   startX ++;
+               }
+               else
+               {
+                   startY ++;
+               }
+
+			 
+			}
+     }
+
+    public void DisplayOcean()
+    {
+
+    }
+
+    public void Target(int x, int y)
+    {
+
+    }
+
+    }
+}
---
+++ battleship 3/battleship 3/Ship.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BattleShip
+{
+    class Ship
+    {
+        public enum ShipType
+        {
+            Carrier, Battleship, Cruiser, Submarine, Minesweeper
+        }
+
+        public ShipType Type { get; set; }
+        public List<Point> OccupiedPoints { get; set; }
+        public int Length { get; set; }
+        public bool IsDestroyed { get {return  } }
+
+        //constructor
+        public Ship(ShipType typeOfShip)
+        {
+            this.OccupiedPoints = new List<Point>();
+            this.Type = typeOfShip;
+            switch (Type)
+            {
+                case ShipType.Carrier:
+                    this.Length = 5;
+                    break;
+                case ShipType.Battleship:
+                    this.Length = 4;
+                    break;
+                case ShipType.Cruiser:
+                    this.Length = 3;
+                    break;
+                case ShipType.Submarine:
+                    this.Length = 3;
+                    break;
+                case ShipType.Minesweeper:
+                    this.Length = 2;
+                    break;
+                default:
+                    break;
+            }
+        }
+    }
+}
